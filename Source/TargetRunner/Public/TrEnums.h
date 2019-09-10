cb6c0a70907621a05cc6f8f37722c8636def395c
kbbits <kevin.r.barrett@gmail.com>
2019-09-10 15:29:19 -0500

    Changed weapons to use goods as ammo. Added a default weapon (weapon mod).

--- Source/TargetRunner/Public/TrEnums.h
+++ Source/TargetRunner/Public/TrEnums.h
@@ -7,3 +7,23 @@ enum class EAmmoType : uint8
 	Bomb 	UMETA(DisplayName = "Bomb"),
 	Sniper	UMETA(DisplayName = "Sniper")
 };
+
+UENUM(BlueprintType)
+enum class ETRAmmoType : uint8
+{
+	AmmoType_None 	UMETA(DisplayName = "None"),
+	AmmoType_Goods 	UMETA(DisplayName = "Goods"),
+	AmmoType_Bomb 	UMETA(DisplayName = "Bomb"),
+	AmmoType_Sniper	UMETA(DisplayName = "Sniper")
+};
+
+
+UENUM(BlueprintType)
+enum class ETRWeaponState : uint8
+{
+	WeaponState_Idle		UMETA(DisplayName = "Idle"),
+	WeaponState_Firing		UMETA(DisplayName = "Firing"),
+	WeaponState_FireBusy	UMETA(DisplayName = "FireBusy"),
+	WeaponState_Reloading	UMETA(DisplayName = "Reloading"),
+	WeaponState_OutOfAmmo	UMETA(DisplayName = "OutOfAmmo")
+};
