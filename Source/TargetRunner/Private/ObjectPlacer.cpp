0523b97e1a12aa5249b54fc5f64b8369bd8a444f
kbbits <kevin.r.barrett@gmail.com>
2020-11-03 14:53:41 -0600

    Finished up basic work on ObjectPlacer. Added them to most prefabs in use.

--- Source/TargetRunner/Private/ObjectPlacer.cpp
+++ Source/TargetRunner/Private/ObjectPlacer.cpp
@@ -3,6 +3,9 @@
 
 #include "ObjectPlacer.h"
 #include "Kismet/GameplayStatics.h"
+#include "Kismet/KismetMathLibrary.h"
+#include "UnrealEd.h"
+#include "ResourceClusterBase.h"
 
 // Sets default values
 AObjectPlacer::AObjectPlacer()
@@ -10,14 +13,20 @@ AObjectPlacer::AObjectPlacer()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = false;
 
+	PlacementDistanceAlongHitNormal = 16.f;
+	BoxExtent = FVector(20.0f, 250.0f, 150.0f);
 	ConeAngle = 35.0f;
-	ConeLength = 500.0f;
+	TargetDistance = 300.0f;
+	MaxTracesPerPlacement = 20;
+	TraceBoxExtents = FVector(0.5f, 0.5f, 0.5f);
 	RootScene = CreateDefaultSubobject<USceneComponent>(TEXT("Root Scene"));
 	SetRootComponent(RootScene);
 	FacingArrow = CreateDefaultSubobject<UArrowComponent>(TEXT("FacingArrow"));
 	FacingArrow->AttachToComponent(RootScene, FAttachmentTransformRules::KeepRelativeTransform);
-	ConeTarget = CreateDefaultSubobject<USphereComponent>(TEXT("EffectTarget"));
+	ConeTarget = CreateDefaultSubobject<USphereComponent>(TEXT("ConeTarget"));
 	ConeTarget->AttachToComponent(FacingArrow, FAttachmentTransformRules::KeepRelativeTransform);
+	BoxTarget = CreateDefaultSubobject<UBoxComponent>(TEXT("BoxTarget"));
+	BoxTarget->AttachToComponent(FacingArrow, FAttachmentTransformRules::KeepRelativeTransform);
 	UpdateComponents();
 }
 
@@ -30,22 +39,29 @@ void AObjectPlacer::BeginPlay()
 
 void AObjectPlacer::UpdateComponents()
 {
-	if (ConeTarget)
+	if (BoxTarget) { BoxTarget->SetVisibility(PlaceIn == EObjectPlacerType::InBox, true); }
+	if (ConeTarget) { ConeTarget->SetVisibility(PlaceIn == EObjectPlacerType::InCone, true); }
+	if (PlaceIn == EObjectPlacerType::InCone && ConeTarget)
 	{
 		if (ConeAngle >= 90.0f) {
-			ConeTarget->SetSphereRadius(ConeLength);
+			ConeTarget->SetSphereRadius(TargetDistance);
 			ConeTarget->SetRelativeLocation(FVector(0.0f, 0.0f, 0.0f));
 		}
 		else if (ConeAngle <= 0.0f) {
 			ConeTarget->SetSphereRadius(1.0f);
-			ConeTarget->SetRelativeLocation(FVector(ConeLength, 0.0f, 0.0f));
+			ConeTarget->SetRelativeLocation(FVector(TargetDistance, 0.0f, 0.0f));
 		}
 		else {
-			float Radius = FMath::Tan(FMath::DegreesToRadians(ConeAngle)) * ConeLength;
+			float Radius = FMath::Tan(FMath::DegreesToRadians(ConeAngle)) * TargetDistance;
 			if (Radius > 5000.0f) { Radius = 5000.0f; }
 			ConeTarget->SetSphereRadius(Radius);
-			ConeTarget->SetRelativeLocation(FVector(ConeLength, 0.0f, 0.0f));
+			ConeTarget->SetRelativeLocation(FVector(TargetDistance, 0.0f, 0.0f));
 		}		
+	}
+	else if (PlaceIn == EObjectPlacerType::InBox && BoxTarget)
+	{
+		BoxTarget->SetRelativeLocation(FVector(TargetDistance, 0.0f, 0.0f));
+		BoxTarget->SetBoxExtent(BoxExtent, false);
 	}
 }
 
@@ -60,9 +76,17 @@ void AObjectPlacer::PostInitProperties()
 void AObjectPlacer::PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent)
 {
 	Super::PostEditChangeProperty(PropertyChangedEvent);
-	if (ConeAngle < 0.0f) { ConeAngle = 0.0f; }
-	if (ConeAngle > 180.0f) { ConeAngle = 180.0f; }
-	if (ConeLength < 0.0f) { ConeLength = 0.0f; }
+	if (PlaceIn == EObjectPlacerType::InCone) {
+		if (ConeAngle < 0.0f) { ConeAngle = 0.0f; }
+		if (ConeAngle > 180.0f) { ConeAngle = 180.0f; }
+	}
+	else if (PlaceIn == EObjectPlacerType::InBox) {
+		if (BoxExtent.X <= 0.0f) { BoxExtent.X = 1.0f; }
+		if (BoxExtent.Y <= 0.0f) { BoxExtent.Y = 1.0f; }
+		if (BoxExtent.Z <= 0.0f) { BoxExtent.Z = 1.0f; }
+		BoxExtent = BoxExtent.BoundToCube(5000.0f);
+	}
+	if (TargetDistance < 0.0f) { TargetDistance = 0.0f; }
 	UpdateComponents();
 }
 #endif
@@ -72,73 +96,123 @@ void AObjectPlacer::PostEditChangeProperty(FPropertyChangedEvent& PropertyChange
 void AObjectPlacer::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
 }
 
 
-AActor* AObjectPlacer::PlaceOne(FRandomStream& RandStream, const AActor* PlacedObjectOwner)
+AActor* AObjectPlacer::PlaceOne(FRandomStream& RandStream, const AActor* PlacedObjectOwner, bool& bPlacedSuccessfully)
 {
 	AActor* TmpPlacedObject = nullptr;
-	FTransform SpawnTransform = GetPlaceTransformInCone(RandStream);
+	FTransform SpawnTransform;
 	FActorSpawnParameters SpawnParams;
 	SpawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
-	SpawnParams.Owner = const_cast<AActor*>(PlacedObjectOwner);
+	if (PlacedObjectOwner) { SpawnParams.Owner = const_cast<AActor*>(PlacedObjectOwner); }
+	SpawnTransform = GetPlaceTransform(RandStream, bPlacedSuccessfully);
+	if (!bPlacedSuccessfully) { return nullptr; }
 	TmpPlacedObject = GetWorld()->SpawnActor<AActor>(ClassToPlace, SpawnTransform, SpawnParams);
-	
 	return TmpPlacedObject;
 }
 
 
-FTransform AObjectPlacer::GetPlaceTransformAtConeCenter()
+#if WITH_EDITOR
+void AObjectPlacer::TryPlaceOne()
 {
-	FTransform PlaceTransform;
+	FRandomStream RandStream;
+	AActor* PlacedActor;
+	AActor* ParentActor = nullptr;
+	bool bSuccess;
+	if (GEditor && GEditor->GetSelectedActorCount() > 1)
+	{
+		USelection* SelectedActors = GEditor->GetSelectedActors();
+		for (FSelectionIterator Iter(*SelectedActors); Iter; ++Iter)
+		{
+			AActor* Actor = Cast<AActor>(*Iter);
+			if (Actor && !Actor->IsA<AObjectPlacer>())
+			{
+				ParentActor = Actor;
+				break;
+			}
+		}
+		if (!IsValid(ParentActor)) { ParentActor = nullptr; }
+	}
+	RandStream.GenerateNewSeed();
+	PlacedActor = PlaceOne(RandStream, ParentActor, bSuccess);
+	FText AttachError;
+	if (PlacedActor && ParentActor && PlacedActor->EditorCanAttachTo(ParentActor, AttachError))
+	{		
+		PlacedActor->AttachToActor(ParentActor, FAttachmentTransformRules::KeepWorldTransform);
+	}
+}
+#endif
+
+
+FTransform AObjectPlacer::GetPlaceTransform(FRandomStream& RandStream, bool& bFound)
+{
+	FTransform PlaceTransform = FTransform::Identity;
+	FVector StartLocation;
+	FVector EndLocation;
+	int32 TraceTries = 0;
+	int32 MaxTraces = MaxTracesPerPlacement > 0 ? MaxTracesPerPlacement : 1;
 	FHitResult Hit;
-	bool bHitSomething;
-	FVector EndTraceLocation = FacingArrow->GetComponentLocation() + (FacingArrow->GetForwardVector() * ConeLength);
-	PlaceTransform.SetIdentity();
-	bHitSomething = GetWorld()->LineTraceSingleByChannel(
-		Hit,
-		FacingArrow->GetComponentLocation(),
-		EndTraceLocation,
-		ECollisionChannel::ECC_WorldDynamic
-	);
-	if (bHitSomething)
+	bool bHitSomething = false;
+	FCollisionShape TraceShape = FCollisionShape::MakeBox(TraceBoxExtents);
+	
+	bFound = false;
+	while (!bHitSomething && TraceTries < MaxTraces)
 	{
-		FVector RotateFilter = FVector(0.0f, (bRotateYToHitSurface ? 1.0f : 0.0f), (bRotateZToHitSurface ? 1.0f : 0.0f));
-		PlaceTransform.SetLocation(Hit.ImpactPoint);
-		Hit.
-		PlaceTransform.SetRotation((Hit.ImpactNormal.Normalize() * RotateFilter).ToOrientationRotator().Quaternion());
+		TraceTries++;
+		switch (PlaceIn)
+		{
+		case EObjectPlacerType::InCone:
+			GetTraceLocationsInCone(RandStream, StartLocation, EndLocation);
+			break;
+		case EObjectPlacerType::InBox:
+			GetTraceLocationsInBox(RandStream, StartLocation, EndLocation);
+			break;
+		}
+		bHitSomething = GetWorld()->SweepSingleByChannel(
+			Hit,
+			StartLocation,
+			EndLocation,
+			UKismetMathLibrary::FindLookAtRotation(StartLocation, EndLocation).Quaternion(),
+			ECollisionChannel::ECC_Visibility,
+			TraceShape
+		);
+		// Don't allow hits to pile up. Try again if we hit actor of type we are placing.
+		if (bHitSomething && Hit.Actor->GetClass()->IsChildOf(ClassToPlace)) { bHitSomething = false; }
+		if (bHitSomething)
+		{
+			FRotator ObjectRotation;
+			FRotator ImpactNormalRotation = Hit.ImpactNormal.ToOrientationRotator();
+			if (bRotateToHitSurface)
+			{
+				ObjectRotation = FRotator(ImpactNormalRotation.Pitch + FMath::FRandRange(-10.0f, 10.0f), ImpactNormalRotation.Yaw + FMath::FRandRange(-10.0f, 10.0f), FMath::FRandRange(MinRandomRoll, MaxRandomRoll));
+			}
+			else
+			{
+				ObjectRotation = FRotator(0.f, 0.f, 0.f);
+			}
+			PlaceTransform.SetLocation(Hit.ImpactPoint + (Hit.ImpactNormal * PlacementDistanceAlongHitNormal));
+			PlaceTransform.SetRotation(ObjectRotation.Quaternion() * InitialRotation.Quaternion());
+			bFound = true;
+			return PlaceTransform;
+		}
 	}
-	else
-	{
-		PlaceTransform.SetLocation(FacingArrow->GetComponentLocation() + (FacingArrow->GetForwardVector() * ConeLength));
-	}
-	return PlaceTransform;
+	return FTransform::Identity;
 }
 
 
-FTransform AObjectPlacer::GetPlaceTransformInCone(UPARAM(ref) FRandomStream& RandStream)
+void AObjectPlacer::GetTraceLocationsInCone(FRandomStream& RandStream, FVector& StartLocation, FVector& EndLocation)
 {
-	FTransform PlaceTransform;
-	FHitResult Hit;
-	bool bHitSomething;
-	FVector EndTraceLocation = FacingArrow->GetComponentLocation() + (RandStream.VRandCone(FacingArrow->GetForwardVector(), FMath::DegreesToRadians(ConeAngle)) * ConeLength);
-	PlaceTransform.SetIdentity();
-	bHitSomething = GetWorld()->LineTraceSingleByChannel(
-		Hit,
-		FacingArrow->GetComponentLocation(),
-		EndTraceLocation,
-		ECollisionChannel::ECC_WorldDynamic
-	);
-	if (bHitSomething)
-	{
-		FVector RotateFilter = FVector(0.0f, (bRotateYToHitSurface ? 1.0f : 0.0f), (bRotateZToHitSurface ? 1.0f : 0.0f));
-		PlaceTransform.SetLocation(Hit.ImpactPoint);
-		PlaceTransform.SetRotation((Hit.ImpactNormal.Normalize() * RotateFilter).ToOrientationRotator().Quaternion());
-	}
-	else
-	{
-		PlaceTransform.SetLocation(FacingArrow->GetComponentLocation() + (FacingArrow->GetForwardVector() * ConeLength));
-	}
-	return PlaceTransform;
+	StartLocation = FacingArrow->GetComponentLocation();
+	EndLocation = StartLocation + (RandStream.VRandCone(FacingArrow->GetForwardVector(), FMath::DegreesToRadians(ConeAngle)) * TargetDistance);
+}
+
+
+void AObjectPlacer::GetTraceLocationsInBox(FRandomStream& RandStream, FVector& StartLocation, FVector& EndLocation)
+{
+	StartLocation = FacingArrow->GetComponentLocation();
+	FVector EndTraceLocation = FVector(TargetDistance + BoxExtent.X, (2 * (RandStream.GetFraction() * BoxExtent.Y)) - BoxExtent.Y, (2 * (RandStream.GetFraction() * BoxExtent.Z)) - BoxExtent.Z);
+	float EndTraceLength = EndTraceLocation.Size(); 
+	FQuat RelativeRotation = FacingArrow->GetRelativeRotationFromWorld(FacingArrow->GetForwardVector().Rotation().Quaternion());
+	EndLocation = StartLocation + FacingArrow->GetComponentToWorld().TransformVector(RelativeRotation.RotateVector(EndTraceLocation.GetSafeNormal()) * EndTraceLength);
 }
--- Source/TargetRunner/Public/ObjectPlacer.h
+++ Source/TargetRunner/Public/ObjectPlacer.h
@@ -6,7 +6,23 @@
 #include "GameFramework/Actor.h"
 #include "Components/ArrowComponent.h"
 #include "Components/SphereComponent.h"
+#include "Components/BoxComponent.h"
 #include "ObjectPlacer.generated.h"
+
+UENUM(BlueprintType)
+enum class EObjectPlacerType : uint8
+{
+	InCone	UMETA(DisplayName = "In Cone"),
+	InBox	UMETA(DisplayName = "In Box")
+};
+
+UENUM(BlueprintType)
+enum class EObjectPlacerAlign : uint8
+{
+	Any				UMETA(DisplayName = "Any"),
+	HorizontalOnly	UMETA(DisplayName = "Horizontal Only"),
+	VerticalOnly	UMETA(DisplayName = "Vertical Only")
+};
 
 UCLASS()
 class TARGETRUNNER_API AObjectPlacer : public AActor
@@ -22,21 +38,62 @@ public:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
 		TSubclassOf<AActor> ClassToPlace;
 
-	// Degrees
+	// The targeting area type to use for placement.
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		EObjectPlacerType PlaceIn;
+
+	// Determines if placement is allowed on vertial surfaces, horizontal surfaces or both.
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		EObjectPlacerAlign PlacementAllowed;
+
+	// When using In Box targeting, this is box extents (the half-size of each dimention).
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (AllowPreserveRation="True"))
+		FVector BoxExtent;
+
+	// When using In Cone targeting, this is the angle of the cone in Degrees
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (UIMin = "1.0", UIMax = "90.0"))
 		float ConeAngle;
 
+	// Distance from placer to trace for placement hits. (i.e. the length of the placement cone or distance of placement box from placer)
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (UIMin = "1.0", UIMax = "10000.0"))
-		float ConeLength;
+		float TargetDistance;
 
+	// An offset distance from the hit, to "push out" (i.e. offset) the spawn location away from the hit location.
+	// Default = 16
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
-		bool bRotateXToHitSurface;
+		float PlacementDistanceAlongHitNormal;
 
+	// Rotation applied to placed actor before any hit/normal rotations are applied.
+	// This initial rotation should result in the "top" of the actor being aligned towards +X.
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
-		bool bRotateYToHitSurface;
+		FRotator InitialRotation;
 
+	// This rotation is in context of the actor being aligned with up towards +X.
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
-		bool bRotateZToHitSurface;
+		float MinRandomRoll;
+
+	// This rotation is in context of the actor being aligned with up towards +X.
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		float MaxRandomRoll;
+
+	// Should the placed actor be rotated so it "sprouts up" from the normal of the surface hit?
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		bool bRotateToHitSurface;
+
+	// The limit of placed objects for this ObjectPlacer instance.
+	// <= 0 is no limit. Default = 0.
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		int32 MaxPlacedObjects;
+
+	// The number of traces placer will fire in target area trying to get a hit on a surface. Default = 20.
+	// If <= 1 it will do one trace
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		int32 MaxTracesPerPlacement;
+
+	// The size of the box used for tracing hits.
+	// Default = (0.5, 0.5, 0.5) = a one meter box.
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		FVector TraceBoxExtents;
 
 protected:
 
@@ -44,12 +101,21 @@ protected:
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
 		USceneComponent* RootScene;
 
+	// The direction to project along.
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
 		UArrowComponent* FacingArrow;
 
+	// A visual aid for where the cone target rests.
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
 		USphereComponent* ConeTarget;
 
+	// A visual aid for where the box target rests.
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+		UBoxComponent* BoxTarget;
+
+	// Current count of objects placed
+	UPROPERTY(BlueprintReadOnly)
+		int32 PlacedObjects;
 
 protected:
 	// Called when the game starts or when spawned
@@ -60,6 +126,7 @@ protected:
 public:	
 	
 	void PostInitProperties() override;
+
 #if WITH_EDITOR
 	void PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent) override;
 #endif
@@ -68,11 +135,19 @@ public:
 	virtual void Tick(float DeltaTime) override;
 
 	UFUNCTION(BlueprintCallable, Meta = (UnsafeDuringActorConstruction = "true"))
-		AActor* PlaceOne(UPARAM(ref) FRandomStream& RandStream, const AActor* PlacedObjectOwner);
+		AActor* PlaceOne(UPARAM(ref) FRandomStream& RandStream, const AActor* PlacedObjectOwner, bool& bPlacedSuccessfully);
+
+#if WITH_EDITOR
+	UFUNCTION(CallInEditor)
+		void TryPlaceOne();
+#endif
 
 	UFUNCTION(BlueprintPure)
-		FTransform GetPlaceTransformAtConeCenter();
+		FTransform GetPlaceTransform(UPARAM(ref) FRandomStream& RandStream, bool& bFound);
 
 	UFUNCTION(BlueprintPure)
-		FTransform GetPlaceTransformInCone(UPARAM(ref) FRandomStream& RandStream);
+		void GetTraceLocationsInCone(UPARAM(ref) FRandomStream& RandStream, FVector& StartLocation, FVector& EndLocation);
+
+	UFUNCTION(BlueprintPure)
+		void GetTraceLocationsInBox(UPARAM(ref) FRandomStream& RandStream, FVector& StartLocation, FVector& EndLocation);
 };
