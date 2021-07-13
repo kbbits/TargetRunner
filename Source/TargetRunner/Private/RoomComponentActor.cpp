ed866b73c90f508be06a5416a12cbacf4beaf581
kbbits <kevin.r.barrett@gmail.com>
2021-07-13 16:57:39 -0500

    Woring on RoomCompoonent collections.

--- Source/TargetRunner/Private/RoomComponentActor.cpp
+++ Source/TargetRunner/Private/RoomComponentActor.cpp
@@ -1,16 +1,155 @@
 
 #include "RoomComponentActor.h"
+#include "TargetRunner.h"
+#include "Kismet/GameplayStatics.h"
+//#include "Kismet/KismetMathLibrary.h"
 
 ARoomComponentActor::ARoomComponentActor()
 {
-	bReplicates = true;
-	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-
-	RootScene = CreateDefaultSubobject<USceneComponent>(TEXT("RootScene"));
-	SetRootComponent(RootScene);
-	
+	bReplicates = false;
 	PrimaryActorTick.bCanEverTick = false;
+	RootScene = CreateDefaultSubobject<USceneComponent>(TEXT("RootScene"));
+	SetRootComponent(RootScene);	
+	bMoveISMCs = false;
+	bCollectionSpawned = false;
 }
+
+
+bool ARoomComponentActor::FilterRCACollections()
+{
+	bool bRemovedAny = false;
+	TArray<FRoomComponentActorCollectionItem> InvalidItems;
+	for (FRoomComponentActorCollection RCACollection : SubRoomComponents)
+	{
+		InvalidItems.Empty();
+		for (FRoomComponentActorCollectionItem RCAItem : RCACollection.RoomComponentActors)
+		{
+			if (!CanContainRoomComponentClass(RCAItem.RCAClass)) {
+				InvalidItems.Add(RCAItem);
+			}
+		}
+		for (FRoomComponentActorCollectionItem RCAItem : InvalidItems)
+		{
+			RCACollection.RoomComponentActors.Remove(RCAItem);
+			bRemovedAny = true;
+		}
+	}
+	if (bRemovedAny) {
+		UE_LOG(LogTRGame, Warning, TEXT("RoomComponentActor FilterRCACollections removed items."))
+	}
+	return bRemovedAny;
+}
+
+
+bool ARoomComponentActor::SpawnRCACollections()
+{
+	if (bCollectionSpawned) {
+		return bCollectionSpawned;
+	}
+	if (!GetWorld()) {
+		return false;
+	}
+	FilterRCACollections();
+	ARoomComponentActorCollectionActor* CollectionActor = nullptr;
+	FTransform SpawnTransform;
+	FActorSpawnParameters SpawnParams;
+	SpawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
+	// Spawn a collection actor for each in our collection
+	for (FRoomComponentActorCollection RCACollection : SubRoomComponents)
+	{
+		SpawnTransform.SetLocation(GetTransform().GetLocation() + RCACollection.LocalTransform.GetLocation());
+		SpawnTransform.SetRotation(GetTransform().TransformRotation(RCACollection.LocalTransform.GetRotation()));
+		SpawnTransform.SetScale3D(GetTransform().GetScale3D());
+		CollectionActor = GetWorld()->SpawnActorDeferred<ARoomComponentActorCollectionActor>(ARoomComponentActorCollectionActor::StaticClass(), SpawnTransform, SpawnParams.Owner, SpawnParams.Instigator, SpawnParams.SpawnCollisionHandlingOverride);
+		if (CollectionActor) {
+			CollectionActor->RoomComponentActorCollection = RCACollection;
+			CollectionActor->RandSeed = RandStream.RandRange(0, INT_MAX - 1);
+			UGameplayStatics::FinishSpawningActor(CollectionActor, CollectionActor->GetTransform());
+			CollectionActor->PickAndInit();
+			SpawnedCollections.Add(CollectionActor);
+			bCollectionSpawned = true;
+		}
+	}
+	return bCollectionSpawned;
+}
+
+
+void ARoomComponentActor::PostInitProperties()
+{
+	Super::PostInitProperties();
+	if (RandStream.GetInitialSeed() != RandSeed) {
+		RandStream.Initialize(RandSeed);
+	}
+	FilterRCACollections();
+	if (GetWorld())
+	{
+		for (ARoomComponentActorCollectionActor* CollectionActor : SpawnedCollections)
+		{
+			if (IsValid(CollectionActor)) {
+				CollectionActor->Destroy();
+			}
+		}
+		bCollectionSpawned = false;
+		SpawnRCACollections();
+	}
+}
+
+
+#if WITH_EDITOR
+void ARoomComponentActor::PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent)
+{
+	Super::PostEditChangeProperty(PropertyChangedEvent);
+	if (RandStream.GetInitialSeed() != RandSeed) {
+		RandStream.Initialize(RandSeed);
+	}
+	FilterRCACollections();
+	if (GetWorld())
+	{
+		for (ARoomComponentActorCollectionActor* CollectionActor : SpawnedCollections)
+		{
+			if (IsValid(CollectionActor)) {
+				CollectionActor->Destroy();
+			}
+		}
+		bCollectionSpawned = false;
+		SpawnRCACollections();
+	}
+}
+#endif
+
+
+void ARoomComponentActor::PostInitializeComponents()
+{
+	Super::PostInitializeComponents();
+
+	SpawnRCACollections();
+}
+
+
+bool ARoomComponentActor::CanContainRoomComponentClass(const TSubclassOf<ARoomComponentActor> ContainClass)
+{
+	if (ContainClass == ARoomComponentActor::StaticClass()) {
+		UE_LOG(LogTRGame, Warning, TEXT("RoomComponentActor sub collection cannot contain RoomComponentActor base class."));
+		return false;
+	}
+	if (ContainClass == GetClass()) {
+		UE_LOG(LogTRGame, Warning, TEXT("RoomComponentActor sub collection cannot contain itself."));
+		return false;
+	}
+	if (ContainClass->IsChildOf(GetClass()))
+	{
+		UE_LOG(LogTRGame, Warning, TEXT("RoomComponentActor sub collection cannot contain child classes of itself."));
+		return false;
+	}
+	return true;
+}
+
+
+void ARoomComponentActor::SetMoveISMCs_Implementation(const bool bMoveThem)
+{
+	bMoveISMCs = bMoveThem;
+}
+
 
 int32 ARoomComponentActor::GetPrimaryISMCs(TArray<UInstancedStaticMeshComponent*>& PrimaryISMCs)
 {
@@ -26,6 +165,7 @@ int32 ARoomComponentActor::GetPrimaryISMCs(TArray<UInstancedStaticMeshComponent*
 	}
 	return PrimaryISMCs.Num();
 }
+
 
 int32 ARoomComponentActor::GetAltISMCs(const UInstancedStaticMeshComponent* ParentISMC, TArray<UInstancedStaticMeshComponent*>& AltISMCs)
 {
@@ -134,4 +274,135 @@ bool ARoomComponentActor::RevertToPrimaryMeshes()
 		}
 	}
 	return AnyAltsRemoved;
+}
+
+/////////////////////////////////////////// ARoomComponentActorCollectionActor ///////////////////////////////////
+
+ARoomComponentActorCollectionActor::ARoomComponentActorCollectionActor()
+{
+	bReplicates = false;
+	PrimaryActorTick.bCanEverTick = false;
+	RootScene = CreateDefaultSubobject<USceneComponent>(TEXT("RootScene"));
+	SetRootComponent(RootScene);    
+	PickedRoomComponentActor_CAC = CreateDefaultSubobject<UChildActorComponent>(TEXT("PickedRoomComponent"));
+	PickedRoomComponentActor_CAC->SetupAttachment(RootScene);
+	bool bFound = false;
+	PickedClass = NULL;
+	GetRoomComponentActorClass(bFound);
+	if (bFound) {
+		PickedRoomComponentActor_CAC->SetChildActorClass(PickedClass);
+		PickedRoomComponentActor_CAC->CreateChildActor();
+	}
+}
+
+//ARoomComponentActorCollectionActor::ARoomComponentActorCollectionActor(const FRoomComponentActorCollection& RCACollection)
+//{
+//	RoomComponentActorCollection = RCACollection;
+//	ARoomComponentActorCollectionActor::ARoomComponentActorCollectionActor();
+//}
+
+
+void ARoomComponentActorCollectionActor::BeginPlay()
+{
+	PickAndInit();
+	if (IsValid(PickedRoomComponentActor_CAC->GetChildActor()))
+	{
+		ARoomComponentActor * ChildRef = Cast<ARoomComponentActor>(PickedRoomComponentActor_CAC->GetChildActor());
+		if (ChildRef) {
+			ChildRef->SetMoveISMCs(true);
+		}
+	}
+}
+
+
+void ARoomComponentActorCollectionActor::PickAndInit()
+{
+	bool bFound = true;
+	if (RandStream.GetInitialSeed() != RandSeed) {
+		RandStream.Initialize(RandSeed);
+		Reset();
+	}	
+	if (!IsValid(PickedClass)) {
+		GetRoomComponentActorClass(bFound);
+	}
+	if (bFound && IsValid(PickedClass) && PickedRoomComponentActor_CAC->GetChildActorClass() != PickedClass) {
+		if (IsValid(PickedRoomComponentActor_CAC->GetChildActor())) {
+			PickedRoomComponentActor_CAC->DestroyChildActor();
+		}
+		PickedRoomComponentActor_CAC->SetChildActorClass(PickedClass);
+		PickedRoomComponentActor_CAC->CreateChildActor();
+	}
+}
+
+
+void ARoomComponentActorCollectionActor::PostInitProperties()
+{
+	Super::PostInitProperties();
+	PickAndInit();
+}
+
+
+#if WITH_EDITOR
+void ARoomComponentActorCollectionActor::PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent)
+{
+	Super::PostEditChangeProperty(PropertyChangedEvent);
+	PickAndInit();
+}
+#endif
+
+
+TSubclassOf<ARoomComponentActor> ARoomComponentActorCollectionActor::PickRoomComponentActorClass(bool& bFoundValid)
+{
+	if (IsValid(PickedClass) && PickedClass != ARoomComponentActor::StaticClass()) {
+		return PickedClass;
+	}
+	if (RoomComponentActorCollection.RoomComponentActors.Num() == 0) {
+		bFoundValid = false;
+		PickedClass = NULL;// ARoomComponentActor::StaticClass();
+		return PickedClass;
+	}
+	float TotalWeight = 0.0f;
+	// Sum total weight
+	for (const FRoomComponentActorCollectionItem CollectionItem : RoomComponentActorCollection.RoomComponentActors)
+	{
+		TotalWeight += FMath::Max(0.0f, CollectionItem.Weight);
+	}
+	// If total weight = 0, pick a random one
+	if (TotalWeight == 0.0f)
+	{
+		bFoundValid = true;
+		PickedClass = RoomComponentActorCollection.RoomComponentActors[RandStream.RandRange(0, RoomComponentActorCollection.RoomComponentActors.Num() - 1)].RCAClass;
+		return PickedClass;
+	}
+	else
+	{	
+		// Pick a target weight and grab the first one in our picked weight range.
+		float PickedWeight = RandStream.FRandRange(0.0f, TotalWeight);
+		TotalWeight = 0.0f;
+		for (const FRoomComponentActorCollectionItem CollectionItem : RoomComponentActorCollection.RoomComponentActors)
+		{
+			TotalWeight += FMath::Max(0.0f, CollectionItem.Weight);
+			if (PickedWeight <= TotalWeight) {
+				bFoundValid = true;
+				PickedClass = CollectionItem.RCAClass;
+				return PickedClass;
+			}
+		}
+	}
+	bFoundValid = false;
+	PickedClass = NULL; // ARoomComponentActor::StaticClass();
+	return PickedClass;
+}
+
+
+TSubclassOf<ARoomComponentActor> ARoomComponentActorCollectionActor::GetRoomComponentActorClass(bool& bFoundValid)
+{
+	PickRoomComponentActorClass(bFoundValid);
+	return PickedClass;
+}
+
+
+void ARoomComponentActorCollectionActor::Reset()
+{
+	PickedClass = NULL;
 }
--- Source/TargetRunner/Public/RoomComponentActor.h
+++ Source/TargetRunner/Public/RoomComponentActor.h
@@ -6,6 +6,57 @@
 #include "GameFramework/Actor.h"
 #include "RoomComponentActor.generated.h"
 
+
+USTRUCT(BlueprintType)
+struct TARGETRUNNER_API FRoomComponentActorCollectionItem
+{
+	GENERATED_BODY()
+
+public:
+
+	UPROPERTY(EditAnywhere, Category = "RoomComponentActor")
+		TSubclassOf<ARoomComponentActor> RCAClass;
+
+	UPROPERTY(EditAnywhere, Category = "RoomComponentActor")
+		float Weight = 1.0f;
+
+	FORCEINLINE bool operator==(const FRoomComponentActorCollectionItem& OtherType) const
+	{
+		if (RCAClass != OtherType.RCAClass) return false;
+		return true;
+	}
+
+	FORCEINLINE bool operator==(const FRoomComponentActorCollectionItem& OtherType)
+	{
+		if (RCAClass != OtherType.RCAClass) return false;
+		return true;
+	}
+
+	FORCEINLINE bool operator==(FRoomComponentActorCollectionItem& OtherType)
+	{
+		if (RCAClass != OtherType.RCAClass) return false;
+		return true;
+	}
+};
+
+
+USTRUCT(BlueprintType)
+struct TARGETRUNNER_API FRoomComponentActorCollection
+{
+	GENERATED_BODY()
+
+public:
+
+	UPROPERTY(EditAnywhere, Category = "RoomComponentActor")
+		TArray<FRoomComponentActorCollectionItem> RoomComponentActors;
+
+	UPROPERTY(EditAnywhere, Category = "RoomComponentActor")
+		FTransform LocalTransform;
+};
+
+
+//////////////////////////////////////  ARoomComponentActor /////////////////////////////
+
 UCLASS()
 class TARGETRUNNER_API ARoomComponentActor : public AActor
 {
@@ -26,7 +77,46 @@ public:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Meta = (ExposeOnSpawn = "true"))
 		float AltMeshPercent;
 
+	// Should this move it's ISMCs to the grid manager?
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+		bool bMoveISMCs;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		TArray<FRoomComponentActorCollection> SubRoomComponents;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Meta = (ExposeOnSpawn = "true"))
+		int32 RandSeed;
+
+protected:
+
+	UPROPERTY()
+		FRandomStream RandStream;
+
+	bool bCollectionSpawned;
+
+	UPROPERTY()
+		TArray<ARoomComponentActorCollectionActor*> SpawnedCollections;
+
+protected:
+
+	bool FilterRCACollections();
+
+	bool SpawnRCACollections();
+
 public:
+	virtual void PostInitProperties() override;
+
+#if WITH_EDITOR
+	virtual void PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent) override;
+#endif
+
+	virtual void PostInitializeComponents() override;
+
+	UFUNCTION(BlueprintCallable)
+		bool CanContainRoomComponentClass(const TSubclassOf<ARoomComponentActor> ContainClass);
+
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable)
+		void SetMoveISMCs(const bool bMoveThem);
 
 	// Get all the primary (i.e. non-alt) InstancedStaticMeshComponents of this Room Component. These are the ISMCs attached directly to the RootScene.
 	// Returns the count of components in PrimaryISMCs.
@@ -48,3 +138,59 @@ public:
 		bool RevertToPrimaryMeshes();
 };
 
+
+///////////////////////////////////////  ARoomComponentActorCollectionActor /////////////////////////////////
+
+UCLASS()
+class TARGETRUNNER_API ARoomComponentActorCollectionActor : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	ARoomComponentActorCollectionActor();
+
+	//ARoomComponentActorCollectionActor(const FRoomComponentActorCollection& RCACollection);
+
+public:
+	UPROPERTY()
+		USceneComponent* RootScene;
+
+	UPROPERTY(VisibleAnywhere)
+		UChildActorComponent* PickedRoomComponentActor_CAC;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "RoomComponentActor")
+		FRoomComponentActorCollection RoomComponentActorCollection;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "RoomComponentActor", Meta = (ExposeOnSpawn = "true"))
+		int32 RandSeed;
+
+protected:
+	UPROPERTY()
+		TSubclassOf<ARoomComponentActor> PickedClass;
+
+	UPROPERTY()
+		FRandomStream RandStream;
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+
+	UFUNCTION(BlueprintCallable, CallInEditor)
+		void PickAndInit();
+
+	virtual void PostInitProperties() override;
+
+#if WITH_EDITOR
+	virtual void PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent) override;
+#endif
+
+	UFUNCTION()
+		TSubclassOf<ARoomComponentActor> PickRoomComponentActorClass(bool& bFoundValid);
+
+	UFUNCTION(BlueprintCallable, Category = "RoomComponentActor")
+		TSubclassOf<ARoomComponentActor> GetRoomComponentActorClass(bool& bFoundValid);
+
+	UFUNCTION(BlueprintCallable, CallInEditor, Category = "RoomComponentActor")
+		void Reset();
+};
